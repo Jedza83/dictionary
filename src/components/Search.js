d7b3878a47d634e46daf61296eddbb4f7e3b043f
Jedza83 <gmejlana@gmail.com>
2023-12-13 10:52:25 +0100

    dictionary

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import { AiOutlineSearch } from 'react-icons/ai';
-import useFetchDictionary from '../hooks/useFetchDictionary';
-import { useDictionaryData } from '../context/DataContext';
+import React, { useEffect, useState } from "react";
+import { AiOutlineSearch } from "react-icons/ai";
+import useFetchDictionary from "../hooks/useFetchDictionary";
+import { useDictionaryData } from "../context/DataContext";
 
 const Search = () => {
   const dictionary = useDictionaryData();
-  const [word, setWord] = useState('hello');
+  const [word, setWord] = useState("");
   const [data] = useFetchDictionary(word, [word]);
 
   useEffect(() => {
@@ -13,16 +13,16 @@ const Search = () => {
   }, [data, word]);
 
   return (
-    <div className='relative bg-slate-100 rounded-2xl'>
+    <div className="relative bg-slate-100 rounded-2xl">
       <input
-        placeholder='Type word here...'
-        className='bg-transparent rounded-2xl	
-       p-3 text-sm w-full'
-        type='text'
+        placeholder="Type word here..."
+        className="bg-transparent rounded-2xl	
+       p-3 text-sm w-full"
+        type="text"
         value={word}
         onChange={(e) => setWord(e.target.value)}
       />
-      <AiOutlineSearch className='absolute right-3 text-violet-500 top-4' />
+      <AiOutlineSearch className="absolute right-3 text-violet-500 top-4" />
     </div>
   );
 };
